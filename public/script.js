8110e50fb6e2bbf38ddc3983b448a795b8cd18db
Glitch (hello-express) <none>
2020-09-16 11:58:00 +0000

    💴☔️ Checkpoint ./public/script.js:22490447/569 ./views/index.html:22490447/20 ./public/style.css:22490447/70

--- public/script.js
+++ public/script.js
@@ -1,7 +1,21 @@
+const main = document.getElementById("main");
 let sounds = [
   'fart',
   'win',
   'lose',
   'correct',
   'wrong',
-]
+];
+
+for (let sound of sounds) {
+  let div = document.createElement("div");
+  let button = document.createElement("button");
+  
+  
+  div.classList.add("sound");
+  button.innerText = sound;
+  div.appendChild(button);
+
+  
+  main.appendChild(div);
+}
