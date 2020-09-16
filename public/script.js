620fba50578c6bed7f5ec7b3a76664ad2a112512
Glitch (soundboard-server) <none>
2020-09-16 13:07:00 +0000

    🚟🎿 Checkpoint ./audio-links.json:22490447/8 ./public/script.js:22490447/938

--- public/script.js
+++ public/script.js
@@ -1,26 +1,28 @@
-const main = document.getElementById("main");
-let sounds = ["monster-kill", "correct", "wrong"];
+(async () => {
+  const main = document.getElementById("main");
+  const audioInfo = await (await fetch("/audio-info.json")).json();
 
-for (let sound of sounds) {
-  let div = document.createElement("div");
-  let button = document.createElement("button");
-  let audio = document.createElement("audio");
-  let source = document.createElement("source");
-  
-  source.src = "/media/" + sound + ".mp3";
-  
-  audio.appendChild(source);
+  for (const sound in audioInfo) {
+    let div = document.createElement("div");
+    let button = document.createElement("button");
+    let audio = document.createElement("audio");
+    let source = document.createElement("source");
+    
+    source.src = audioInfo[sound].src;
+    
+    audio.appendChild(source);
 
-  button.innerText = sound;
-  button.addEventListener('click', () => {
-    audio.currenTime = 0;
-    audio.play();
-  });
-  
-  div.classList.add("sound");
-  div.appendChild(button);
-  div.appendChild(audio);
+    button.innerText = sound;
+    button.addEventListener('click', () => {
+      audio.currenTime = 0;
+      audio.play();
+    });
+    
+    div.classList.add("sound");
+    div.appendChild(button);
+    div.appendChild(audio);
 
-  
-  main.appendChild(div);
-}
+    
+    main.appendChild(div);
+  }
+})();
