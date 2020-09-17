1200ad7e91e61517d1e4729b457d7e037598477c
Dario-0 <48695966+Dario-0@users.noreply.github.com>
2020-09-17 15:18:33 +0200

    Room ID is now not shown by default, can be with checkbox

--- public/script.js
+++ public/script.js
@@ -1,15 +1,25 @@
 const socket = io();
 
 const main = document.getElementById("main");
-const roomIdElement = document.getElementById("roomIdDisplay");
+const roomIdElement = document.getElementById("roomIdElement");
+const roomIdDisplay = document.getElementById("roomIdDisplay");
+const roomIdInput = document.getElementById("roomId");
 let roomId;
 
 const urlSearch = new URLSearchParams(window.location.search);
+roomIdElement.style.display = roomIdDisplay.checked ? "initial" : "none";
+roomIdInput.type = roomIdDisplay.checked ? "text" : "password";
 if (urlSearch.get('roomId')) {
   roomIdElement.innerText += urlSearch.get('roomId');
 } else {
   roomIdElement.innerText = "You are currently in the public room";
 }
+
+roomIdDisplay.addEventListener('change', () => {
+  roomIdElement.style.display = roomIdDisplay.checked ? "initial" : "none";
+  roomIdInput.type = roomIdDisplay.checked ? "text" : "password";
+});
+
 roomId = urlSearch.get('roomId');
 
 socket.on('connect', () => {
@@ -31,22 +41,24 @@ socket.on('reciveSound', (data) => {
     let button = document.createElement("button");
     let audio = document.createElement("audio");
     let source = document.createElement("source");
-    
+
     source.src = audioInfo[soundId].src;
-    
+
     audio.appendChild(source);
     audio.id = soundId;
 
     button.innerText = audioInfo[soundId].name;
     button.addEventListener('click', () => {
-      socket.emit('sendSound', {soundId});
+      socket.emit('sendSound', {
+        soundId
+      });
     });
-    
+
     div.classList.add("sound");
     div.appendChild(button);
     div.appendChild(audio);
 
-    
+
     main.appendChild(div);
   }
   setGridColumns();
@@ -55,7 +67,7 @@ socket.on('reciveSound', (data) => {
 window.addEventListener('resize', setGridColumns);
 
 function setGridColumns() {
-  let nOfButtonsPerRow = Math.floor((window.innerWidth - 70)/(main.firstChild.clientWidth + 50));
+  let nOfButtonsPerRow = Math.floor((window.innerWidth - 70) / (main.firstChild.clientWidth + 50));
   // console.log(nOfButtonsPerRow);
   main.style.gridTemplateColumns = "10rem ".repeat(nOfButtonsPerRow).trim();
 }
