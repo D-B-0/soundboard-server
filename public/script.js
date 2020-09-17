dc7dfb9e9967a8314064386ae2a3c1f4e986ab7d
Glitch (soundboard-server) <none>
2020-09-17 09:22:01 +0000

    🎶 Made with Glitch

---
+++ public/script.js
@@ -0,0 +1,67 @@
+const socket = io();
+
+const main = document.getElementById("main");
+const roomIdElement = document.getElementById("roomIdDisplay");
+let roomId;
+
+const urlSearch = new URLSearchParams(window.location.search);
+if (urlSearch.get('roomId')) {
+  roomIdElement.innerText += urlSearch.get('roomId');
+} else {
+  roomIdElement.innerText = "You are currently in the public room";
+}
+roomId = urlSearch.get('roomId');
+
+socket.on('connect', () => {
+  console.log("Connceted");
+});
+
+socket.emit('connect-to-room', roomId);
+
+socket.on('reciveSound', (data) => {
+  console.log("Playing sound with id:", data.soundId);
+  playAudio(data.soundId);
+});
+
+(async () => {
+  const audioInfo = await (await fetch("/audio-info.json")).json();
+
+  for (const soundId in audioInfo) {
+    let div = document.createElement("div");
+    let button = document.createElement("button");
+    let audio = document.createElement("audio");
+    let source = document.createElement("source");
+    
+    source.src = audioInfo[soundId].src;
+    
+    audio.appendChild(source);
+    audio.id = soundId;
+
+    button.innerText = audioInfo[soundId].name;
+    button.addEventListener('click', () => {
+      socket.emit('sendSound', {soundId});
+    });
+    
+    div.classList.add("sound");
+    div.appendChild(button);
+    div.appendChild(audio);
+
+    
+    main.appendChild(div);
+  }
+  setGridColumns();
+})();
+
+window.addEventListener('resize', setGridColumns);
+
+function setGridColumns() {
+  let nOfButtonsPerRow = Math.floor((window.innerWidth - 70)/(main.firstChild.clientWidth + 50));
+  // console.log(nOfButtonsPerRow);
+  main.style.gridTemplateColumns = "10rem ".repeat(nOfButtonsPerRow).trim();
+}
+
+function playAudio(id) {
+  let audio = document.getElementById(id);
+  audio.currentTime = 0;
+  audio.play();
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+
+io.on('connection', (socket) => {
+  console.log("A user connected");
+  socket.on('connect-to-room', (roomId) => {
+    socket.join(roomId);
+    socket.on('sendSound', (soundInfo) => {
+      console.log(`Sending sound with id "${soundInfo.soundId}" to clients in room "${roomId}"`);
+      io.to(roomId).emit('reciveSound', soundInfo);
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + process.env.PORT);
+});
